b3e7ce01f8f71b6482354aa7e5ee5fdcb48eddd9
Jorlex22 <alexveros46@gmail.com>
2025-01-03 05:05:48 +0700

    feat: readmi

--- hono-cli/src/cli.ts
+++ hono-cli/src/cli.ts
@@ -4,12 +4,49 @@ import chalk from 'chalk'
 import { initializeProject } from './generators/init.generator'
 import { generateModule } from './generators/module.generator'
 import { generateRouter } from './generators/router.generator'
+import path from 'path'
+import fs from 'fs/promises'
 
 const program = new Command()
 
 program
     .version('1.0.0')
-    .description('Hono.js project generator')
+    .description('Hono.js project generator');
+
+program
+    .command('version')
+    .alias('v')
+    .description('Check package version')
+    .action(async () => {
+        try {
+            // Read package.json
+            const packagePath = path.join(process.cwd(), 'package.json')
+            const packageContent = await fs.readFile(packagePath, 'utf-8')
+            const packageJson = JSON.parse(packageContent)
+
+            console.log(chalk.blue('Package Information:'))
+            console.log(chalk.green(`Name: ${packageJson.name}`))
+            console.log(chalk.green(`Version: ${packageJson.version}`))
+
+            // Show dependencies versions if they exist
+            if (packageJson.dependencies) {
+                console.log(chalk.blue('\nDependencies:'))
+                Object.entries(packageJson.dependencies).forEach(([name, version]) => {
+                    console.log(chalk.green(`${name}: ${version}`))
+                })
+            }
+
+            // Show devDependencies versions if they exist
+            if (packageJson.devDependencies) {
+                console.log(chalk.blue('\nDev Dependencies:'))
+                Object.entries(packageJson.devDependencies).forEach(([name, version]) => {
+                    console.log(chalk.green(`${name}: ${version}`))
+                })
+            }
+        } catch (error) {
+            console.error(chalk.red('Error reading package.json:'), error)
+        }
+    });
 
 program
     .command('init <projectName>')
